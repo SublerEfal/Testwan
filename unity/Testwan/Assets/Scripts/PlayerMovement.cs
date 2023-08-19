9b0e3d232b266bb9715ce56993adcd895a2ce141
SublerEfal <sam.peter.vr@gmail.com>
2023-08-19 17:56:44 +0200

    Fix all the weird stuff that Cristian broke

--- unity/Testwan/Assets/Scripts/PlayerMovement.cs
+++ unity/Testwan/Assets/Scripts/PlayerMovement.cs
@@ -2,41 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 5f;
+    private static readonly float diagonalModifier = 0.71f;
+
+    [SerializeField] private float speed = 5f;
+
     private Rigidbody2D rigidBody;
     private PlayerSpawn spawn;
     private Killable killable;
     private Animator animator;
-    Vector2 movement;
-    // Start is called before the first frame update
+
     void Start()
     {
         this.rigidBody = GetComponent<Rigidbody2D>();
         this.spawn = FindObjectOfType<PlayerSpawn>();
         this.killable = GetComponent<Killable>();
+        this.animator = GetComponent<Animator>();
        
         this.killable.SetOnKillHandler(this.OnKilled);
 
         this.Respawn();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement.Normalize();
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
     void FixedUpdate()
     {
-        rigidBody.MovePosition(rigidBody.position + movement * speed * Time.fixedDeltaTime);
+        float xSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float ySpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        if (ySpeed != 0f && xSpeed != 0f)
+        {
+            ySpeed *= diagonalModifier;
+            xSpeed *= diagonalModifier;
+        }
+
+        Vector2 newVelocity = new Vector2(this.rigidBody.velocity.x + xSpeed, this.rigidBody.velocity.y + ySpeed);
+        this.rigidBody.velocity = newVelocity;
+
+        animator.SetFloat("Horizontal", newVelocity.x);
+        //Making the y slightly smaller than the x will cause the animation to only look horizontally when walking diagonally.
+        animator.SetFloat("Vertical", newVelocity.y * 0.99f);
+        animator.SetFloat("Speed", newVelocity.sqrMagnitude);
     }
 
     private void OnKilled()
@@ -52,6 +60,6 @@ public class PlayerMovement : MonoBehaviour
 
     void Respawn()
     {
-        //this.transform.position = this.spawn.transform.position;
+        this.transform.position = this.spawn.transform.position;
     }
 }
